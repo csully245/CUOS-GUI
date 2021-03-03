76d53237144a9d90f6952dc5885a9b220bc4251e
Cate Sullivan <csully245@gmail.com>
2021-03-03 14:26:40 -0500

    Fix bugs: sizing and file exclusion

--- Helpers.py
+++ Helpers.py
@@ -3,34 +3,33 @@ import winsound
 import numpy as np
 from PIL import Image, ImageTk, ImageGrab, ImageOps
 from matplotlib import pyplot as plt
-from matplotlib.figure import Figure 
-from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,
-                                               NavigationToolbar2Tk)
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib import cm
 import json
 import os
 import shutil
 from datetime import date
-from time import strftime
 import time
 import pyautogui
 import threading
 
-#-------------------------------------------------
+# -------------------------------------------------
 # Constants
-#-------------------------------------------------
+# -------------------------------------------------
 
 default_img_path = "assets/CUOS-med.png"
 date_default = {
-        "year": "0001",
-        "month": "01",
-        "day": "01"
-        }
+    "year": "0001",
+    "month": "01",
+    "day": "01"
+}
 default_filename = "setup.json"
 
-#-------------------------------------------------
+
+# -------------------------------------------------
 # Generic Data Manipulation
-#-------------------------------------------------
+# -------------------------------------------------
 
 def get_suffix(word, delimeter):
     ''' Returns the part of word after the last instance of 'delimeter' '''
@@ -38,13 +37,15 @@ def get_suffix(word, delimeter):
         word = word.partition(delimeter)[2]
     return word
 
-#-------------------------------------------------
+
+# -------------------------------------------------
 # Message Windows
-#-------------------------------------------------
+# -------------------------------------------------
 class Notice_Window:
     '''
     GUI template for notice messages, such as progress updates
     '''
+
     def __init__(self, txt):
         self.root = tk.Tk()
         self.root.title("Notice")
@@ -54,10 +55,12 @@ class Notice_Window:
         self.lbl.pack(padx=10, pady=10)
         self.root.mainloop()
 
+
 class Error_Window:
     '''
     GUI template for error messages
     '''
+
     def __init__(self, txt):
         self.root = tk.Tk()
         self.root.title("Error")
@@ -68,18 +71,20 @@ class Error_Window:
         winsound.PlaySound("SystemExit", winsound.SND_ASYNC)
         self.root.mainloop()
 
-#-------------------------------------------------
+
+# -------------------------------------------------
 # Image display
-#-------------------------------------------------
+# -------------------------------------------------
 
 def rgb2gray(rgb):
     '''
     Converts the 3-channel rgb image into grayscale
     Developed by Yong Ma for Daq_Yong_v2.py
     '''
-    r, g, b = rgb[:,:,0] , rgb[:,:,1] , rgb[:,:,2]
-    gray  = 0.2989 * r + 0.587 * g + 0.114 * b
+    r, g, b = rgb[:, :, 0], rgb[:, :, 1], rgb[:, :, 2]
+    gray = 0.2989 * r + 0.587 * g + 0.114 * b
     return gray
+
 
 def resize_image(img, k, ratio, base):
     '''
@@ -104,8 +109,9 @@ def resize_image(img, k, ratio, base):
         out = Image.fromarray(out)
     return out
 
+
 def load_image(img_path, root, k=1.0, ratio=2.0, base=200, recolor=False,
-                vmin=0, vmax=255):
+               vmin=0, vmax=255):
     '''
     Plots image using ImageTk.PhotoImage
     
@@ -132,7 +138,8 @@ def load_image(img_path, root, k=1.0, ratio=2.0, base=200, recolor=False,
     img = ImageTk.PhotoImage(img)
     return (tk.Label(root, image=img), img)
 
-def plot_image(img_path, root, k=1.0, ratio=2.0, base=300, recolor=False,
+
+def plot_image(img_path, root, k=1.0, ratio=1.75, base=-1, recolor=False,
                colormap=cm.magma, vmin=0, vmax=255, flipud=False):
     '''
     Plots image using plt.imshow and tk.Canvas
@@ -156,21 +163,21 @@ def plot_image(img_path, root, k=1.0, ratio=2.0, base=300, recolor=False,
     # Edit plot settings
     base_size = 2.5
     fig, plot1 = plt.subplots(1, subplot_kw={'aspect': 'auto'},
-                    figsize=(ratio*base_size, base_size))
+                              figsize=(ratio * base_size, base_size))
     xleft, xright = plot1.get_xlim()
     ybottom, ytop = plot1.get_ylim()
-    plot1.set_aspect(abs((xright-xleft)/(ybottom-ytop))*ratio)
+    plot1.set_aspect(abs((xright - xleft) / (ybottom - ytop)) * ratio)
     plot1.tick_params(axis='both', which='both',
-                    bottom=False, left=False, labelbottom=False,
-                    labelleft=False)
+                      bottom=False, left=False, labelbottom=False,
+                      labelleft=False)
     img_arr = np.asarray(img)
-    if (len(img_arr.shape) > 2):
+    if len(img_arr.shape) > 2:
         img_arr = rgb2gray(img_arr)
-    if (flipud):
+    if flipud:
         img_arr = np.flipud(img_arr)
-    if (recolor):
+    if recolor:
         plot1.imshow(img_arr, vmin=vmin, vmax=vmax, cmap=colormap,
-                                    rasterized=True, aspect='auto')
+                     rasterized=True, aspect='auto')
     else:
         plot1.imshow(img_arr, rasterized=True, aspect='auto')
 
@@ -178,8 +185,12 @@ def plot_image(img_path, root, k=1.0, ratio=2.0, base=300, recolor=False,
     canvas = FigureCanvasTkAgg(fig, master=root)
     canvas.draw()
     canvas = canvas.get_tk_widget()
-    canvas.configure(width=ratio*base, height=base)
-    return (canvas, fig)
+    if base == -1:
+        dimensions = get_from_file("dimensions", "assets/dimensions.json")
+        base = dimensions["img_size"]
+    canvas.configure(width=ratio * base, height=base)
+    return canvas, fig
+
 
 def delete_img(img):
     '''
@@ -193,6 +204,7 @@ def delete_img(img):
         plt.close(img)
     else:
         del img
+
 
 def max_num_in_dir(path):
     '''
@@ -215,6 +227,7 @@ def max_num_in_dir(path):
     else:
         return max(nums)
 
+
 def to_3_digit(num):
     '''
     Returns a 3-digit string of the input positive int
@@ -233,6 +246,7 @@ def to_3_digit(num):
     else:
         return "00" + str(num)
 
+
 def get_shot_num(path):
     ''' Returns the shot number (in str) of an image at path 'path' '''
     if ("_s" in path):
@@ -244,9 +258,10 @@ def get_shot_num(path):
     out = out.partition(".")[0]
     return out
 
-#-------------------------------------------------
+
+# -------------------------------------------------
 # File management
-#-------------------------------------------------
+# -------------------------------------------------
 
 def get_from_file(key, filename=default_filename):
     '''
@@ -259,6 +274,7 @@ def get_from_file(key, filename=default_filename):
         Error_Window('Key "' + key + '" does not exist.')
         return ""
     return data[key]
+
 
 def edit_file(key, value, filename=default_filename):
     '''
@@ -274,7 +290,9 @@ def edit_file(key, value, filename=default_filename):
     with open(filename, "w") as write_file:
         json.dump(data, write_file)
 
-def save_most_recent(src, dest, diag, num):
+
+def save_most_recent(src, dest, diag, num,
+                     extensions=["png", "jpg", "jpeg", "tif", "tiff"]):
     '''
     Copies the most recently edited file in the source directory into the
     destination
@@ -284,12 +302,15 @@ def save_most_recent(src, dest, diag, num):
     -dest: string, destination directory
     -diag: string, name of diagnostic
     -num: int/string, shot number
+    -extensions: list of strings, supported file types
     '''
     # Identify most recent file
     files = os.listdir(src)
     new_files = []
     for file in files:
-        new_files.append(src + "/" + file)
+        ext = get_suffix(file, ".").lower()
+        if ext in extensions:
+            new_files.append(src + "/" + file)
     path = max(new_files, key=os.path.getctime)
     num = to_3_digit(num)
 
@@ -299,6 +320,7 @@ def save_most_recent(src, dest, diag, num):
     name = diag + "_" + today.strftime("%Y%m%d")
     name += "_s" + to_3_digit(num) + "." + ext
     shutil.copy(path, os.path.join(dest, name))
+
 
 def get_today():
     '''
@@ -327,26 +349,27 @@ def save_plots(num, shotrundir, delay=0.8):
 
     # Define threads
     def thread_a(x_button, y_button):
-        ''' Switches tab to Recent Image Display '''
+        """ Switches tab to Recent Image Display """
         width, height = pyautogui.size()
         width *= x_button
         height *= y_button
         pyautogui.click(width, height, button="left")
-    
-    def thread_b(num, shotrundir, delay, left, right,
-                top, bottom, x_button, y_button):
-        ''' Saves screenshot '''
+
+    def thread_b(num, shotrundir, delay, x_button, y_button):
+        """ Saves screenshot """
         # Delay to leave time for tab switching
         time.sleep(delay)
 
         # Take and crop screenshot
         img = ImageGrab.grab()
+        '''
         width, height = img.size
         left = int(width * left)
         right = int(width * right)
         top = int(height * top)
         bottom = int(height * bottom)
         img = img.crop((left, top, right, bottom))
+        '''
 
         # Save screenshot
         dest = os.path.join(shotrundir, "Aggregated Plots")
@@ -356,23 +379,21 @@ def save_plots(num, shotrundir, delay=0.8):
         ext = ".png"
         # Check for duplicates
         tag = "_v"
-        num = 1
-        if (os.path.isfile(os.path.join(dest, filename) + ext)):
+        if os.path.isfile(os.path.join(dest, filename) + ext):
             # Add "_v#" tag for alternate save
             base = os.path.join(dest, filename)
             base = base.partition(ext)[0]
             tag = "_v"
             num = 1
-            while (os.path.isfile(base + tag + str(num) + ext)):
+            while os.path.isfile(base + tag + str(num) + ext):
                 num += 1
             filename += tag + str(num)
         filename += ext
         # Save
         img.save(os.path.join(dest, filename), format="PNG")
-    
+
     # Get dimensions
     '''
-    -left, right, top, bottom: float, ratio of screen to use for that margin
     -x_button, y_button: float, ration of screen to use to press the button
         for Recent Image Display
     
@@ -381,15 +402,11 @@ def save_plots(num, shotrundir, delay=0.8):
     coordinates of a certain mouse position
     '''
     dimensions = get_from_file("dimensions", "./assets/dimensions.json")
-    left = dimensions["left"]
-    right = dimensions["right"]
-    top = dimensions["top"]
-    bottom = dimensions["bottom"]
     x_button = dimensions["x_button"]
     y_button = dimensions["y_button"]
 
     # Operate
     t1 = threading.Thread(target=thread_b, args=(num, shotrundir, delay,
-                    left, right, top, bottom, x_button, y_button))
+                                                 x_button, y_button))
     t1.start()
     thread_a(x_button, y_button)
