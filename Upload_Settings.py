ad8d56f4c8e2b5d24addfd953468a46ad77e96f8
Cate Sullivan <csully245@gmail.com>
2021-04-14 00:30:14 -0400

    Add upload functionality and custom entries

--- Upload_Settings.py
+++ Upload_Settings.py
@@ -3,7 +3,9 @@ from tkinter import ttk
 from tkinter.scrolledtext import ScrolledText
 from tkinter import filedialog as fd
 import os
-from datetime import date
+import boto3
+from botocore.exceptions import NoCredentialsError
+import json
 
 import Helpers
 
@@ -24,17 +26,18 @@ class UI(tk.Frame):
         self.fr_artifacts = tk.LabelFrame(self.fr_left, text="Shot Artifacts")
         self.fr_parameters = tk.LabelFrame(self.fr_center, text="Shot Parameters")
         self.fr_metrics = tk.LabelFrame(self.fr_right, text="Shot Metrics")
+        self.fr_custom = tk.LabelFrame(self.fr_right, text="Custom Data")
 
         self.fr_shot_id.grid(row=0, column=0, sticky=tk.N)
         self.fr_artifacts.grid(row=1, column=0, sticky=tk.N, pady=5)
         self.fr_parameters.grid(row=0, column=0, sticky=tk.N)
         self.fr_metrics.grid(row=0, column=0, sticky=tk.N)
+        self.fr_custom.grid(row=1, column=0, sticky=tk.N, pady=5)
+
+        self.btn_test = tk.Button(self, command=lambda: self.get_manual_data())
+        self.btn_test.grid(row=2, column=0)
 
         self.total_width = 65
-
-        # Remove after testing
-        self.btn_test = tk.Button(self.fr_artifacts, text="Test", command=lambda: self.get_settings())
-        self.btn_test.grid(row=10, column=0)
 
         """ Shot ID """
         self.lbl_exp_name = tk.Label(self.fr_shot_id, text="Experiment Name")
@@ -216,12 +219,12 @@ class UI(tk.Frame):
 
         self.lbl_xray_filter_id = tk.Label(self.fr_metrics_1, text="X-Ray Filter ID")
         self.lbl_xray_filter_thickness = tk.Label(self.fr_metrics_1, text="X-Ray Filter Thickness (mm)")
-        self.entry_xray_filter_id = tk.Entry(self.fr_metrics_1, width=int(0.5*self.total_width))
-        self.entry_xray_filter_thickness = tk.Entry(self.fr_metrics_1, width=int(0.5*self.total_width))
+        self.entry_xray_filter_id = tk.Entry(self.fr_metrics_1, width=int(0.5 * self.total_width))
+        self.entry_xray_filter_thickness = tk.Entry(self.fr_metrics_1, width=int(0.5 * self.total_width))
         self.lbl_dm1_actuator_voltage = tk.Label(self.fr_metrics_1, text="DM1 Actuator Voltage (V)")
         self.lbl_dm2_actuator_voltage = tk.Label(self.fr_metrics_1, text="DM2 Actuator Voltage (V)")
-        self.entry_dm1_actuator_voltage = tk.Entry(self.fr_metrics_1, width=int(0.5*self.total_width))
-        self.entry_dm2_actuator_voltage = tk.Entry(self.fr_metrics_1, width=int(0.5*self.total_width))
+        self.entry_dm1_actuator_voltage = tk.Entry(self.fr_metrics_1, width=int(0.5 * self.total_width))
+        self.entry_dm2_actuator_voltage = tk.Entry(self.fr_metrics_1, width=int(0.5 * self.total_width))
 
         self.lbl_xray_filter_id.grid(row=0, column=0)
         self.lbl_xray_filter_thickness.grid(row=0, column=1)
@@ -239,9 +242,9 @@ class UI(tk.Frame):
         self.lbl_pmt_1 = tk.Label(self.fr_metrics_2, text="PMT 1 (V)")
         self.lbl_pmt_2 = tk.Label(self.fr_metrics_2, text="PMT 2 (V)")
         self.lbl_pmt_3 = tk.Label(self.fr_metrics_2, text="PMT 3 (V)")
-        self.entry_pmt_1 = tk.Entry(self.fr_metrics_2, width=int(1/3*self.total_width))
-        self.entry_pmt_2 = tk.Entry(self.fr_metrics_2, width=int(1/3*self.total_width))
-        self.entry_pmt_3 = tk.Entry(self.fr_metrics_2, width=int(1/3*self.total_width))
+        self.entry_pmt_1 = tk.Entry(self.fr_metrics_2, width=int(1 / 3 * self.total_width))
+        self.entry_pmt_2 = tk.Entry(self.fr_metrics_2, width=int(1 / 3 * self.total_width))
+        self.entry_pmt_3 = tk.Entry(self.fr_metrics_2, width=int(1 / 3 * self.total_width))
         self.lbl_dazzler_2nd_order_phase = tk.Label(self.fr_metrics_2, text="Dazzler 2nd Order Phase")
         self.lbl_dazzler_3rd_order_phase = tk.Label(self.fr_metrics_2, text="Dazzler 3rd Order Phase")
         self.lbl_dazzler_4th_order_phase = tk.Label(self.fr_metrics_2, text="Dazzler 4th Order Phase")
@@ -297,6 +300,28 @@ class UI(tk.Frame):
         self.btn_upload.grid(row=2, column=0, pady=2)
         self.fr_file_frames.grid(row=3, column=0)
 
+        """ Custom Data """
+        self.fr_custom_1 = tk.Frame(self.fr_custom)
+        self.fr_custom_1.pack()
+        self.fr_custom_2 = tk.Frame(self.fr_custom)
+        self.fr_custom_2.pack(pady=10)
+
+        self.custom_entries = []
+
+        self.lbl_custom_entries = tk.Label(self.fr_custom_1, text="New Entry Title")
+        self.entry_title = tk.Entry(self.fr_custom_1, width=int(0.7 * self.total_width))
+        self.btn_add_custom = tk.Button(self.fr_custom_1, text="Add", command=lambda: self.add_entry(),
+                                        width=int(0.2 * self.total_width))
+        self.lbl_custom_entries.grid(row=1, column=0)
+        self.entry_title.grid(row=1, column=0, padx=10)
+        self.btn_add_custom.grid(row=1, column=1)
+
+    def add_entry(self):
+        new_row = self.CustomEntryFrame(self.fr_custom_2, total_width=self.total_width,
+                                        title=self.entry_title.get())
+        self.custom_entries.append(new_row)
+        new_row.pack()
+
     def select_files(self):
         """ Prompts user to select additional files to upload """
         selected = fd.askopenfilenames(initialdir="./", title="Select Additional Artifacts")
@@ -306,8 +331,8 @@ class UI(tk.Frame):
             new_file_frame.pack()
             self.file_frames.append(new_file_frame)
 
-    def get_settings(self):
-        """ Data to return """
+    def get_manual_data(self):
+        """ Returns the shot id, parameters, and metrics """
         shot_id = {
             "exp_name": self.entry_exp_name.get(),
             "shot_num": self.entry_shot_num.get(),
@@ -351,16 +376,22 @@ class UI(tk.Frame):
             "dazzler_hole_width": self.entry_dazzler_hole_width.get(),
             "dazzler_central_wavelength": self.entry_dazzler_central_wavelength.get(),
         }
-        artifact_files = []
-        for frame in self.file_frames:
-            artifact_files.append(frame.filename)
-        artifacts = {
-            "comments": self.text_comments.get("1.0", tk.END),
-            "filenames": artifact_files
-        }
+
+        """ Add custom entries """
+        for entry in self.custom_entries:
+            data = entry.get()
+            if not data[1] and not data[2]:
+                continue
+            out_data = {data[1]: data[2]}
+            if data[0] == "Shot ID":
+                shot_id.update(out_data)
+            elif data[0] == "Parameter":
+                shot_id.update(out_data)
+            elif data[0] == "Metric":
+                metrics.update(out_data)
 
         """ Convert numbers: int if possible, then float if possible """
-        out = (shot_id, parameters, metrics, artifacts)
+        out = (shot_id, parameters, metrics)
         for dictionary in out:
             for key, value in zip(dictionary.keys(), dictionary.values()):
                 try:
@@ -376,14 +407,72 @@ class UI(tk.Frame):
                         pass
                 except TypeError:
                     pass
-        print(out[0])
-        print(out[1])
-        print(out[2])
-        print(out[3])
+        for i in out:
+            print(out)
         return out
+
+    def get_artifacts(self):
+        """ Returns the location of each selected artifact """
+        artifact_files = []
+        for frame in self.file_frames:
+            artifact_files.append(frame.filename)
+        comments_filename = "comments" + self.entry_exp_name.get() + "_" \
+                            + str(self.entry_shot_num.get()) + "_" \
+                            + self.entry_shot_date.get() + "_" \
+                            + self.entry_shot_time.get() + ".txt"
+        shotrundir = Helpers.get_from_file("shotrundir")
+        comments_filename = os.path.join(shotrundir, comments_filename)
+        with (comments_filename, "w") as write_file:
+            write_file.write(self.text_comments.get("1.0", tk.END))
+        artifact_files.append(comments_filename)
+        return artifact_files
+
+    def upload(self):
+        """ Uploads selected data to AWS s3 bucket """
+        # Get s3 data
+        s3_data = Helpers.get_from_file("s3_data", "s3_data.json")
+        access_key = s3_data["access_key"]
+        secret_key = s3_data["secret_key"]
+        bucket = s3_data["bucket"]
+
+        # Create s3 filename
+        s3_file = self.entry_exp_name.get() + "_"
+        s3_file += str(self.entry_shot_num.get()) + "_"
+        s3_file += self.entry_shot_date.get() + "_"
+        s3_file += self.entry_shot_time.get()
+
+        # Identify local files
+        manual_data = self.get_manual_data()
+        shotrundir = Helpers.get_from_file("shotrundir")
+        filename = os.path.join(shotrundir, "manual_data.json")
+        with (filename, "w") as write_file:
+            json.dump(manual_data, write_file)
+        files = [filename] + self.get_artifacts()
+
+        # Upload
+        successes = 0
+        for file in files:
+            s3 = boto3.client('s3', aws_access_key_id=access_key,
+                              aws_secret_access_key=secret_key)
+            try:
+                s3.upload_file(file[0], bucket, file[1])
+                successes += 1
+            except FileNotFoundError:
+                pass
+            except NoCredentialsError:
+                Helpers.Error_Window("Credentials not available")
+                break
+        if successes == len(files):
+            text = "Successfully uploaded " + str(successes) + " files."
+            Helpers.Notice_Window(text)
+        else:
+            text = "Successfully uploaded " + str(successes) + " files."
+            text += "\nFailed to upload " + str(len(files) - successes) + " files."
+            Helpers.Error_Window(text)
 
     class FileFrame(tk.Frame):
         """ Frame to show an individual selected artifact """
+
         def __init__(self, master, filename, text_width, **options):
             tk.Frame.__init__(self, master, **options)
             self.filename = filename
@@ -404,3 +493,25 @@ class UI(tk.Frame):
 
             self.lbl_filename.grid(row=0, column=0, sticky=tk.W)
             self.btn_remove.grid(row=0, column=1, sticky=tk.E)
+
+    class CustomEntryFrame(tk.Frame):
+        """ Frame to represent user-customized data """
+        def __init__(self, master, title, total_width, **options):
+            tk.Frame.__init__(self, master, **options)
+            self.title = title
+
+            self.lbl_title = tk.Label(self, text=title)
+            self.type_options = ["Shot ID", "Metric", "Parameter", ""]
+            self.type = tk.StringVar()
+            self.type.set(self.type_options[0])
+            self.dropdown_type = ttk.Combobox(self, textvariable=self.type, width=int(0.3 * total_width))
+            self.dropdown_type['values'] = tuple(self.type_options)
+            self.entry_data = tk.Entry(self, width=int(0.6 * total_width))
+
+            self.lbl_title.grid(row=0, column=0, sticky=tk.W)
+            self.dropdown_type.grid(row=1, column=0, padx=5)
+            self.entry_data.grid(row=1, column=1)
+
+        def get(self):
+            """ Returns (title, type, data)"""
+            return self.type.get(), self.title, self.entry_data.get()
